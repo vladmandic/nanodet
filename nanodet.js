31da3ce8f7e9f8391d5c517389073ddc86e8e8c2
Vladimir Mandic <mandic00@live.com>
2021-03-23 13:01:54 -0400

    update

--- nanodet.js
+++ nanodet.js
@@ -7,9 +7,9 @@ const canvas = require('canvas');
 const { labels } = require('./coco-labels');
 
 const modelOptions = {
-  modelPath: 'file://model-tfjs-graph-m/nanodet.json',
-  minScore: 0.10, // low confidence, but still remove irrelevant
-  iouThreshold: 0.10, // be very aggressive with removing overlapped boxes
+  modelPath: 'file://model-tfjs-graph-t/nanodet.json',
+  minScore: 0.23, // low confidence, but still remove irrelevant
+  iouThreshold: 0.5, // percentage when removing overlapped boxes
   maxResults: 20, // high number of results, but likely never reached
   scaleBox: 2.5, // increase box size
 };
@@ -19,19 +19,25 @@ async function saveImage(img, res) {
   // create canvas
   const c = new canvas.Canvas(img.inputShape[0], img.inputShape[1]);
   const ctx = c.getContext('2d');
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = 'white';
-  ctx.fillStyle = 'white';
-  ctx.font = 'small-caps 20px "Segoe UI"';
 
   // load and draw original image
   const original = await canvas.loadImage(img.fileName);
   ctx.drawImage(original, 0, 0, c.width, c.height);
+  // const fontSize = Math.trunc(c.width / 50);
+  const fontSize = Math.round(Math.sqrt(c.width));
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'white';
+  ctx.fillStyle = 'white';
+  ctx.font = `${fontSize}px "Segoe UI"`;
 
   // draw all detected objects
   for (const obj of res) {
     // draw label at center
-    ctx.fillText(`${Math.round(100 * obj.score)}% [${obj.strideSize}] #${obj.class} ${obj.label}`, obj.box[0] + 4, obj.box[1] + 20);
+    // ctx.fillText(`${Math.round(100 * obj.score)}% [${obj.strideSize}] #${obj.class} ${obj.label}`, obj.box[0] + 4, obj.box[1] - 2);
+    ctx.fillStyle = 'black';
+    ctx.fillText(`${Math.round(100 * obj.score)}% ${obj.label}`, obj.box[0] + 5, obj.box[1] - 3);
+    ctx.fillStyle = 'white';
+    ctx.fillText(`${Math.round(100 * obj.score)}% ${obj.label}`, obj.box[0] + 4, obj.box[1] - 4);
     // draw rect using x,y,h,w
     ctx.rect(obj.box[0], obj.box[1], obj.box[2] - obj.box[0], obj.box[3] - obj.box[1]);
   }
@@ -66,50 +72,58 @@ async function loadImage(fileName, inputSize) {
 // process model results
 async function processResults(res, inputSize, outputShape) {
   let results = [];
+  let id = 0;
   for (const strideSize of [1, 2, 4]) { // try each stride size as it detects large/medium/small objects
     // find scores, boxes, classes
     tf.tidy(() => { // wrap in tidy to automatically deallocate temp tensors
       const baseSize = strideSize * 13; // 13x13=169, 26x26=676, 52x52=2704
       // find boxes and scores output depending on stride
-      // log.info('Variation:', strideSize, 'strides', baseSize, 'baseSize');
-      const scores = res.find((a) => (a.shape[1] === (baseSize ** 2) && a.shape[2] === 80))?.squeeze();
-      const features = res.find((a) => (a.shape[1] === (baseSize ** 2) && a.shape[2] === 32))?.squeeze();
-      // log.state('Found features tensor:', features?.shape);
-      // log.state('Found scores tensor:', scores?.shape);
-      const scoreIdx = scores.argMax(1).dataSync(); // location of highest scores
-      const scoresMax = scores.max(1).dataSync(); // values of highest scores
-      const boxesMax = features.reshape([-1, 4, 8]); // reshape [32] to [4,8] where 8 is change of different features inside stride
+      log.info('Variation:', strideSize, 'strides', baseSize, 'baseSize');
+      const scoresT = res.find((a) => (a.shape[1] === (baseSize ** 2) && a.shape[2] === 80))?.squeeze();
+      const featuresT = res.find((a) => (a.shape[1] === (baseSize ** 2) && a.shape[2] < 80))?.squeeze();
+      log.state('Found features tensor:', featuresT?.shape);
+      log.state('Found scores tensor:', scoresT?.shape);
+      // const scoreIdx = scores.argMax(1).dataSync(); // location of highest scores for a given stride
+      // const scoresMax = scores.max(1).dataSync(); // values of highest scores for a given stride
+      const boxesMax = featuresT.reshape([-1, 4, featuresT.shape[1] / 4]); // reshape [32] to [4,8] where 8 is change of different features inside stride
       const boxIdx = boxesMax.argMax(2).arraySync(); // what we need is indexes of features with highest scores, not values itself
-      for (let i = 0; i < scores.shape[0]; i++) {
-        if (scoreIdx[i] !== 0 && scoresMax[i] > modelOptions.minScore) {
-          const cx = (0.5 + Math.trunc(i % baseSize)) / baseSize; // center.x normalized to range 0..1
-          const cy = (0.5 + Math.trunc(i / baseSize)) / baseSize; // center.y normalized to range 0..1
-          const boxOffset = boxIdx[i].map((a) => a * (baseSize / strideSize / inputSize)); // just grab indexes of features with highest scores
-          let boxRaw = [ // results normalized to range 0..1
-            cx - (modelOptions.scaleBox / strideSize * boxOffset[0]),
-            cy - (modelOptions.scaleBox / strideSize * boxOffset[1]),
-            cx + (modelOptions.scaleBox / strideSize * boxOffset[2]),
-            cy + (modelOptions.scaleBox / strideSize * boxOffset[3]),
-          ];
-          boxRaw = boxRaw.map((a) => Math.max(0, Math.min(a, 1))); // fix out-of-bounds coords
-          const box = [ // results normalized to input image pixels
-            boxRaw[0] * outputShape[0],
-            boxRaw[1] * outputShape[1],
-            boxRaw[2] * outputShape[0],
-            boxRaw[3] * outputShape[1],
-          ];
-          const result = {
-            score: scoresMax[i],
-            strideSize,
-            class: scoreIdx[i] + 1,
-            label: labels[scoreIdx[i]].label,
-            center: [Math.trunc(outputShape[0] * cx), Math.trunc(outputShape[1] * cy)],
-            centerRaw: [cx, cy],
-            box: box.map((a) => Math.trunc(a)),
-            boxRaw,
-          };
-          results.push(result);
+      const scores = scoresT.arraySync();
+      for (let i = 0; i < scoresT.shape[0]; i++) {
+        for (let j = 0; j < scoresT.shape[1]; j++) {
+          if (scores[i][j] > modelOptions.minScore) {
+            const cx = (0.5 + Math.trunc(i % baseSize)) / baseSize; // center.x normalized to range 0..1
+            const cy = (0.5 + Math.trunc(i / baseSize)) / baseSize; // center.y normalized to range 0..1
+            const boxOffset = boxIdx[i].map((a) => a * (baseSize / strideSize / inputSize)); // just grab indexes of features with highest scores
+            let boxRaw = [ // results normalized to range 0..1
+              cx - (modelOptions.scaleBox / strideSize * boxOffset[0]),
+              cy - (modelOptions.scaleBox / strideSize * boxOffset[1]),
+              cx + (modelOptions.scaleBox / strideSize * boxOffset[2]),
+              cy + (modelOptions.scaleBox / strideSize * boxOffset[3]),
+            ];
+            boxRaw = boxRaw.map((a) => Math.max(0, Math.min(a, 1))); // fix out-of-bounds coords
+            const box = [ // results normalized to input image pixels
+              boxRaw[0] * outputShape[0],
+              boxRaw[1] * outputShape[1],
+              boxRaw[2] * outputShape[0],
+              boxRaw[3] * outputShape[1],
+            ];
+            const result = {
+              id: id++,
+              strideSize,
+              score: scores[i][j],
+              class: j + 1,
+              label: labels[j].label,
+              center: [Math.trunc(outputShape[0] * cx), Math.trunc(outputShape[1] * cy)],
+              centerRaw: [cx, cy],
+              box: box.map((a) => Math.trunc(a)),
+              boxRaw,
+            };
+            results.push(result);
+          }
         }
+        /*
+        if (scoreIdx[i] !== 0 && scoresMax[i] > modelOptions.minScore) {
+        */
       }
     });
   }
@@ -119,17 +133,15 @@ async function processResults(res, inputSize, outputShape) {
 
   // normally nms is run on raw results, but since boxes need to be calculated this way we skip calulcation of
   // unnecessary boxes and run nms only on good candidates (basically it just does IOU analysis as scores are already filtered)
-  const nmsBoxes = results.map((a) => a.boxRaw);
+  const nmsBoxes = results.map((a) => a.box);
   const nmsScores = results.map((a) => a.score);
   const nms = await tf.image.nonMaxSuppressionAsync(nmsBoxes, nmsScores, modelOptions.maxResults, modelOptions.iouThreshold, modelOptions.minScore);
   const nmsIdx = nms.dataSync();
   tf.dispose(nms);
-
   // filter & sort results
   results = results
     .filter((a, idx) => nmsIdx.includes(idx))
     .sort((a, b) => b.score - a.score);
-
   return results;
 }
 
@@ -145,6 +157,8 @@ async function main() {
   // load model
   const model = await tf.loadGraphModel(modelOptions.modelPath);
   log.info('Loaded model', modelOptions, 'tensors:', tf.engine().memory().numTensors, 'bytes:', tf.engine().memory().numBytes);
+  // @ts-ignore
+  log.info('Model Signature', model.signature);
 
   // load image and get approprite tensor for it
   const inputSize = Object.values(model.modelSignature['inputs'])[0].tensorShape.dim[2].size;
